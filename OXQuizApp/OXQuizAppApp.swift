ae9f3b08393f2bff748d69e161f8b50f455cb24d
Jin Park <jinpark1504@gmail.com>
2024-04-15 15:12:23 +0900

    Initial Commit

---
+++ OXQuizApp/OXQuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OXQuizAppApp.swift
+//  OXQuizApp
+//
+//  Created by Jungjin Park on 2024-04-15.
+//
+
+import SwiftUI
+
+@main
+struct OXQuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
